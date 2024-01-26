1c6af0d40032e79cb3844f838c29caa25118f764
Magdalena Rozpara <magda.rozpara96@gmail.com>
2024-01-26 18:53:33 +0100

    Add password match check functionality

--- javascript.js
+++ javascript.js
@@ -1,20 +1,28 @@
+const pwdContainerEl = document.querySelector(".pwd-input-container")
 const pwdInputEl = document.getElementById("pwd");
 const pwdConfirmEl = document.getElementById("confirm-pwd");
 
 
 function checkMatch(pwd, pwdConfirm) {
-    if (pwd.value === pwdConfirm.value) {
-        console.log("SUCCESS");
-        toggleClass(pwd);
-    } else {
+    if (pwd.value !== pwdConfirm.value) {
         console.log("password do not match");
+        addErrorClass(pwd);
+        addErrorClass(pwdConfirm);
+    } else {
+        console.log("SUCCESS");
+        removeErrorClass(pwd);
+        removeErrorClass(pwdConfirm);
     }
 }
 
-function toggleClass(inputField) {
-    inputField.classList.add("cute");
-    inputField.style.backgroundColor = "blue";
+function addErrorClass(inputField) {
+    inputField.classList.add("error");
 }
+
+function removeErrorClass(inputField) {
+    inputField.classList.remove("error");
+}
+
 
 pwdConfirmEl.addEventListener("input", () => {
     checkMatch(pwdInputEl, pwdConfirmEl);
